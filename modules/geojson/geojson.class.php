55147b559760eee73a324a5179f2eeb7c7980dce
perpinux <perpinux@localhost>
2016-07-11 18:36:40 +0200

    Création du gabarit geojson

--- modules/geojson/geojson.class.php
+++ modules/geojson/geojson.class.php
@@ -11,17 +11,99 @@
 
 class geojson {
     
+    /*
+     * Cette section concerne les gabarits pour les fichiers geojson.
+     */
     
-    public function liste_participants($evenement, $pdo) {
+    var $tableau_geojson = "\t\t{
+                        \"type\": \"Feature\",
+                        \"id\": \"node/{{id}}\",
+                        \"properties\": {
+                          \"@id\": \"node/{{id}}\",
+                          \"vehicule\": \"{{texte_vehicule}}\"
+                        },
+                        \"geometry\": {
+                          \"type\": \"Point\",
+                          \"coordinates\": [
+                            {{lng}} ,
+                            {{lat}}
+                          ]
+                        }
+                      },\n\r";
+    
+    
+    /*
+     * Cette section concerne les méthodes pour générer les listes de participants.
+     */
+    
+    public function liste_participants($id_evenement, $dbh, $error) {
         //Cette méthode sert à générer la liste de tous les participants à un événement.
+        
+        //Requête SQl pour récupérer les coordonnées GPS de l'événement.
+        $sql_evenement = "SELECT * FROM `".PREFIXE."participants`"
+                . "JOIN `".PREFIXE."relations` "
+                . "ON `".PREFIXE."participants`.`id_participant` = `".PREFIXE."relations`.`id_participant` "
+                . "WHERE`".PREFIXE."relations`.`id_evenement` = :id_evenement";
+        //Tableau des variables pour les requêtes SQL.
+        $array_variables = [
+            ":id_evenement" => $id_evenement,
+        ];
+     
+        //Exécution de la requête.
+        try {
+            $resultat_evenement = $dbh->prepare($sql_evenement);
+            $resultat_evenement -> execute($array_variables);
+            $donnees = $resultat_evenement -> fetchAll();
+
+
+            //Contenu du tableau.
+            $tableau_geojson = NULL;
+            foreach ($donnees as $key) {
+                //Données pour le tableay GEOJSON.
+                $id = $key['id_participant'];
+                $lat = $key['latitude'];
+                $lng = $key['longitude'];
+                $vehicule = $key['vehicule'];
+                //Texte pour le véhicule.
+                if ($vehicule == 0){
+                   $texte_vehicule = "Pas de véhicule ";
+                } else  {
+                    $texte_vehicule = "A un véhicule";
+                }
+
+                $tableau_geojson .= "\t\t{
+                        \"type\": \"Feature\",
+                        \"id\": \"node/".$id."\",
+                        \"properties\": {
+                          \"@id\": \"node/".$id."\",
+                          \"vehicule\": \"".$texte_vehicule."\"
+                        },
+                        \"geometry\": {
+                          \"type\": \"Point\",
+                          \"coordinates\": [
+                            $lng ,
+                            $lat
+                          ]
+                        }
+                      },\n\r"; 
+            }
+
+
+
+        } catch (Exception $ex) {
+            echo $error;
+        }
+        
+        
+        
     }
     
-    public function liste_voitures($evenement, $pdo) {
+    public function liste_voitures($id_evenement, $dbh) {
         //Cette méthode sert à générer la liste des participants ayant une voiture.
     
     }
     
-    public function liste_pietons($evenement, $pdo) {
+    public function liste_pietons($id_evenement, $dbh) {
         //Cette méthode sert à générer la liste des participants n'ayant pas de voiture.
     }
     
