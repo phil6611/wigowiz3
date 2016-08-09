547918639991cd60ac4a30a4cde947476eed9c1f
perpinux <perpinux@localhost>
2016-08-09 03:18:22 +0200

    initial commit

---
+++ modules/compte/compte.class.php
@@ -0,0 +1,41 @@
+<?php
+
+
+/**
+ * Description of compte
+ *
+ * @author Philippe Poisse
+ * @version 20160809
+ */
+
+
+class compte {
+    
+    public function create($array) {
+        //Cette méthode sert à créer un compte.
+        
+        
+    }
+    
+    public function read($id) {
+        //Cette méthode sert à lire les informations d'un compte
+    }
+    
+    public function read_pwd($id) {
+        //Cette méthode sert à lire le mot de passe associé à un compte
+    }
+    
+    public function update($array) {
+        //Cette méthode sert à mettre à jour un compte.
+    }
+    
+    public function update_pwd($array) {
+        //Cette méthode sert à mettre à jour un mot de passe.
+    }
+    
+    public function delete($id) {
+        //Cette méthode sert à supprimer définitivement un compte.
+    }
+    
+    
+}
